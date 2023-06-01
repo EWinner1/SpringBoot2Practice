8e009e3f72e8d298520f9ed0813be408b0ea8487
EWinner1 <858564882@qq.com>
2023-06-01 16:13:23 +0800

    Add myBatis plus CRUD

--- SSMPTest/src/test/java/com/ewinner/ssmptest/SSMPApplicationTests.java
+++ SSMPTest/src/test/java/com/ewinner/ssmptest/SSMPApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ewinner.ssmptest;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ewinner.ssmptest.Common.Daos.StaffDao;
@@ -12,65 +13,7 @@ import java.util.List;
 
 @SpringBootTest
 class SSMPApplicationTests {
-
-    @Autowired
-    private StaffDao staffDao;
     @Test
     void contextLoads() {
-        getStaffByIdTest();
-        saveStaffTest();
-        updateStaffTest();
-        deleteStaffTest();
-    }
-    @Test
-    void saveStaffTest(){
-        Staff staff = new Staff();
-        staff.setName("Tony Stark");
-        staff.setSex("male");
-        staff.setDescription("I am Airman");
-        staff.setStaffLevel(1);
-        staffDao.insert(staff);
-        System.out.println("Save success!\n" + staff);
-    }
-    @Test
-    void deleteStaffTest(){
-        Integer id = 43;
-        Staff staff = staffDao.selectById(id);
-        if (staff == null){
-            System.out.println("查无此人");
-        }
-        else {
-            System.out.println(staff);
-            staffDao.deleteById(id);
-            System.out.println("Staff has been removed");
-        }
-    }
-    @Test
-    void updateStaffTest(){
-        Staff staff = staffDao.selectById(41);
-        staff.setDescription("I am Ironman");
-        staffDao.updateById(staff);
-        System.out.println("Update success\n" + staff);
-    }
-    @Test
-    void getAllStaffTest(){
-        List<Staff> staffs = staffDao.selectList(null);
-        System.out.println(staffs.toString().replaceAll("(?<=\\)), ", "\n"));
-    }
-    @Test
-    void getStaffByIdTest(){
-        Integer id = 41;
-        Staff staff = staffDao.selectById(id);
-        System.out.println(staff == null ? "id: " + id + "查无此人" : staff);
-    }
-
-    @Test
-    void getByPage(){
-        IPage<Staff> page0 = new Page<>(0, 7);
-        IPage<Staff> page1 = new Page<>(1, 7);
-        IPage<Staff> page2 = new Page<>(2, 7);
-        staffDao.selectPage(page0, null);
-        staffDao.selectPage(page1, null);
-        System.out.println(staffDao.selectPage(page2, null).getRecords().toString().replaceAll("(?<=\\)), ", "\n"));
     }
 }
---
+++ SSMPTest/src/test/java/com/ewinner/ssmptest/StaffTest.java
@@ -0,0 +1,111 @@
+package com.ewinner.ssmptest;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ewinner.ssmptest.Common.Daos.StaffDao;
+import com.ewinner.ssmptest.Common.Models.Staff;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class StaffTest {
+    @Autowired
+    private StaffDao staffDao;
+    @Test
+    void oneClickTest() {
+        getStaffByIdTest();
+        saveStaffTest();
+        updateStaffTest();
+        deleteStaffTest();
+    }
+    @Test
+    void saveStaffTest(){
+        Staff staff = new Staff();
+        staff.setName("Tony Stark");
+        staff.setSex("male");
+        staff.setDescription("I am Airman");
+        staff.setStaffLevel(1);
+        staffDao.insert(staff);
+        System.out.println("Save success!\n" + staff);
+        System.out.println("ID: " + getUserId(staff));
+    }
+    @Test
+    void deleteStaffTest(){
+        Integer id = getUserId("Tony Stark", "male", 1);
+        Staff staff = staffDao.selectById(id);
+        if (staff == null){
+            System.out.println("查无此人");
+        }
+        else {
+            staffDao.deleteById(id);
+            System.out.println("Staff has been removed");
+        }
+    }
+    @Test
+    void updateStaffTest(){
+        Integer id = getUserId("Tony Stark", "male", 1);
+        Staff staff = staffDao.selectById(id);
+        staff.setDescription("I am Ironman");
+        staffDao.updateById(staff);
+        System.out.println("Update success!\n" + staff);
+    }
+    @Test
+    void getAllStaffTest(){
+        List<Staff> staffs = staffDao.selectList(null);
+        System.out.println(staffs.toString().replaceAll("(?<=\\)), ", "\n"));
+    }
+    @Test
+    void getStaffByIdTest(){
+        Integer id = getUserId("Tony Stark", "male", 1);
+        Staff staff = null;
+        if (id != null){
+            staff = staffDao.selectById(id);
+        }
+        System.out.println(staff == null ? "id: " + id + "查无此人" : staff);
+    }
+
+    @Test
+    void getPage(){
+        IPage<Staff> page0 = new Page<>(0, 7);
+        IPage<Staff> page1 = new Page<>(1, 7);
+        IPage<Staff> page2 = new Page<>(2, 7);
+        staffDao.selectPage(page0, null);
+        staffDao.selectPage(page1, null);
+        System.out.println(staffDao.selectPage(page2, null).getRecords().toString().replaceAll("(?<=\\)), ", "\n"));
+    }
+
+    @Test
+    void getOnCondition(){
+        QueryWrapper<Staff> employQueryWrapper = new QueryWrapper<>();
+        employQueryWrapper.in("staffLevel", 10, 11, 12, 13);
+        System.out.println("Employee list:\n" + staffDao.selectList(employQueryWrapper).toString().replaceAll("(?<=\\)), ", "\n"));
+        QueryWrapper<Staff> bossQueryWrapper = new QueryWrapper<>();
+        bossQueryWrapper.in("StaffLevel", 1, 2, 3);
+        System.out.println("Boss list:\n" + staffDao.selectList(bossQueryWrapper).toString().replaceAll("(?<=\\)), ", "\n"));
+    }
+
+    Integer getUserId(String name, String sex, Integer staffLevel){
+        QueryWrapper<Staff> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq(true, "Name", "Tony Stark");
+        queryWrapper.eq(true, "Sex", "Male");
+        queryWrapper.eq(true, "StaffLevel", 1);
+        return (staffDao.selectOne(queryWrapper) == null) ? null : staffDao.selectOne(queryWrapper).getId();
+    }
+
+    Integer getUserId(Staff staff){
+        LambdaQueryWrapper<Staff> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(staff.getName() == null, Staff::getName, staff.getName());
+        lambdaQueryWrapper.eq(staff.getSex() == null, Staff::getSex, staff.getSex());
+        lambdaQueryWrapper.eq(staff.getStaffLevel() == null, Staff::getStaffLevel, staff.getStaffLevel());
+        //QueryWrapper<Staff> queryWrapper = new QueryWrapper<>();
+        //queryWrapper.eq(true, "Name", staff.getName());
+        //queryWrapper.eq(true, "Sex", staff.getSex());
+        //queryWrapper.eq(true, "StaffLevel", staff.getStaffLevel());
+        return staffDao.selectOne(lambdaQueryWrapper).getId();
+    }
+}
